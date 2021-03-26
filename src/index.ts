2b26de29864d5b8926bf7c84abfe95b76ccecdc3
Alan Pita <pitaman512@gmail.com>
2021-03-26 15:18:37 -0500

    Refactored into one-class-per-source

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,14 @@ export const factory = new Elevated.Factory([
     ... Outcome.builders,
 ]);
 
+export * from './Action';
+export * from './Actor';
+export * from './Effect';
+export * from './Listener';
+export * from './Local';
+export * from './Remote';
+export * from './Resource';
+export * from './State';
+
 export * as Contact from './Contact';
 export * as Outcome from './Outcome';
